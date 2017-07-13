de10099741fdf2bacf6853548b9d7d6156a3e77e
Melson Zacharias <mzacharias@perleybrook.com>
2017-07-13 22:05:17 +0530

    refactored controllers as class

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -3,31 +3,37 @@
 import * as async from "async";
 import * as request from "request";
 import * as graph from "fbgraph";
-import {Response, Request, NextFunction} from "express";
+import { Response, Request, NextFunction } from "express";
 
+class API {
+  constructor() {
+  }
 
-/**
- * GET /api
- * List of API examples.
- */
-export let getApi = (req: Request, res: Response) => {
-  res.render("api/index", {
-    title: "API Examples"
-  });
-};
-
-/**
- * GET /api/facebook
- * Facebook API example.
- */
-export let getFacebook = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.user.tokens.find((token: any) => token.kind === "facebook");
-  graph.setAccessToken(token.accessToken);
-  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-    if (err) { return next(err); }
-    res.render("api/facebook", {
-      title: "Facebook API",
-      profile: results
+  /**
+   * GET /api
+   * List of API examples.
+   */
+  public getApi(req: Request, res: Response) {
+    res.render("api/index", {
+      title: "API Examples"
     });
-  });
-};
+  }
+  /**
+   * GET /api/facebook
+   * Facebook API example.
+   */
+  public getFacebook(req: Request, res: Response, next: NextFunction) {
+    const token = req.user.tokens.find((token: any) => token.kind === "facebook");
+    graph.setAccessToken(token.accessToken);
+    graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
+      if (err) { return next(err); }
+      res.render("api/facebook", {
+        title: "Facebook API",
+        profile: results
+      });
+    });
+  }
+}
+const api = new API();
+export default api;
+
