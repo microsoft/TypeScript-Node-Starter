2570c56c693b7010909ed5112790dd9a601eb9be
kevguy <kevlai22@uw.edu>
2017-07-19 17:15:21 +0800

    fix: fix for tslint

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,9 +2,9 @@ import * as async from "async";
 import * as crypto from "crypto";
 import * as nodemailer from "nodemailer";
 import * as passport from "passport";
-import {default as User, UserModel, AuthToken} from "../models/User";
-import {Request, Response, NextFunction} from "express";
-import {LocalStrategyInfo} from "passport-local";
+import { default as User, UserModel, AuthToken } from "../models/User";
+import { Request, Response, NextFunction } from "express";
+import { LocalStrategyInfo } from "passport-local";
 import { WriteError } from "mongodb";
 const request = require("express-validator");
 
