7d27c2d463b8c31cb798f30e49439a05dcb0e660
kevguy <kevlai22@uw.edu>
2017-07-19 17:15:21 +0800

    fix: fix for tslint

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 
 /**
  * GET /
@@ -9,4 +9,3 @@ export let index = (req: Request, res: Response) => {
     title: "Home"
   });
 };
-
