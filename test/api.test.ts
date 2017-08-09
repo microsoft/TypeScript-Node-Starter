ff6d059921f942ff3ecf634c52b2d129f32856c9
Daniel Temme <dtemme@gmail.com>
2017-08-09 13:27:36 +0200

    change tests to not always pass

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,5 +1,7 @@
+import {} from "jest";
 import * as supertest from "supertest";
-import * as app from "../src/server";
+
+const request = supertest("http://localhost:3000");
 
 describe("GET /api", () => {
   const request = supertest(app);
