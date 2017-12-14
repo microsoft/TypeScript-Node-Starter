98d1641711473760f9dd2a82196fe1c6c9053992
Bowden <wilkelly@microsoft.com>
2017-12-14 15:27:52 -0800

    fix up tests

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,18 +1,16 @@
-import * as supertest from "supertest";
-import * as app from "../src/server";
-
-const request = supertest(app);
+import * as request from "supertest";
+import * as app from "../src/app";
 
 describe("GET /login", () => {
   it("should return 200 OK", () => {
-    return request.get("/login")
+    return request(app).get("/login")
       .expect(200);
   });
 });
 
 describe("GET /signup", () => {
   it("should return 200 OK", () => {
-    return request.get("/signup")
+    return request(app).get("/signup")
       .expect(200);
   });
 });
