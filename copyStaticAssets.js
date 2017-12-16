bfd74b2dc64539359d17ae9fd83dfb19c3c668c0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-12-16 22:41:40 +0100

    Support all-typescript tooling with ts-node
    The ts-node allows to execute build scripts written in TypeScript
    so the entire project could be written in TypeScript.
    This commit introduces support for ts-node, updates packages
    and migrate copy routine to TypeScript
    
    Thanks!

--- copyStaticAssets.js
+++
@@ -1,5 +0,0 @@
-var shell = require('shelljs');
-
-shell.cp('-R', 'src/public/js/lib', 'dist/public/js/');
-shell.cp('-R', 'src/public/fonts', 'dist/public/');
-shell.cp('-R', 'src/public/images', 'dist/public/');
