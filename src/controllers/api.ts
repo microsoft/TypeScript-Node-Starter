be5db9692a9d2afa6b30ad3d226d67a62c1eb521
Bowden <wilkelly@microsoft.com>
2018-01-03 14:15:04 -0800

    remove facebook stuff

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,7 +2,6 @@
 
 import * as async from "async";
 import * as request from "request";
-import * as graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
 
 
@@ -16,18 +15,3 @@ export let getApi = (req: Request, res: Response) => {
   });
 };
 
-/**
- * GET /api/facebook
- * Facebook API example.
- */
-export let getFacebook = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.user.tokens.find((token: any) => token.kind === "facebook");
-  graph.setAccessToken(token.accessToken);
-  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-    if (err) { return next(err); }
-    res.render("api/facebook", {
-      title: "Facebook API",
-      profile: results
-    });
-  });
-};
