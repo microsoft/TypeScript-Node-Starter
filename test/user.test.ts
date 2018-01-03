0513c926f022c6268fe84bd2d4ba41027c087f64
Irene (XiangYi) Chen <xiangyi99@live.com>
2018-01-03 18:12:41 -0500

    coverage/chai support and some unit tests
    1. added jest coverage support (#64)
    2. added chai testing support
    3. created new unit tests

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,6 +1,9 @@
 import * as request from "supertest";
 import * as app from "../src/app";
 
+var chai = require('chai');
+var expect = chai.expect;
+
 describe("GET /login", () => {
   it("should return 200 OK", () => {
     return request(app).get("/login")
@@ -12,5 +15,20 @@ describe("GET /signup", () => {
   it("should return 200 OK", () => {
     return request(app).get("/signup")
       .expect(200);
+  });
+});
+
+
+describe("POST /login", () => {
+  it("should return some defined error message with valid parameters", (done) => {
+    return request(app).post("/login")
+      .field("email", "john@me.com")
+      .field("password", "Hunter2")
+      .expect(302)
+      .end(function(err, res) {
+        expect(res.error).not.to.be.undefined;
+        done();
+      });
+   
   });
 });
