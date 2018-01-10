97b1939c384bf4785034310067ed9d8a16b8a92f
Bowden <wilkelly@microsoft.com>
2018-01-10 09:28:58 -0800

    WIP - still need to use NO_DATABASE env var to safely avoid db errors

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ mongoose.connect(mongoUrl, {useMongoClient: true}).then(
   () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
 ).catch(err => {
   console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-  // process.exit();
+  process.exit();
 });
 
 // Express configuration
--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -5,13 +5,20 @@ import * as  fs from "fs";
 if (fs.existsSync(".env")) {
     logger.debug("Using .env file to supply config environment variables");
     dotenv.config({ path: ".env" });
-} else {
+} else if (fs.existsSync(".env.example")) {
     logger.debug("Using .env.example file to supply config environment variables");
     dotenv.config({ path: ".env.example" });  // you can delete this after you create your own .env file!
+} else {
+    logger.error("This project requires a .env file to configure environment variables");
 }
 export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 
+// Delete this after you've added a database to the project
+if (process.env["NO_DATABASE_SETUP"] === "true") {
+    logger.debug(`No database detected. Either follow steps in the README to set up a MongoDB,
+    or delete the NO_DATABASE_SETUP setting from your .env settings file if you already have one setup.`);
+}
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
 export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : process.env["MONGODB_URI_LOCAL"];
 
