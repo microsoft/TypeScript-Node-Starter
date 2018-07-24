da1b9be09dec54431da390ef26772e752be3f60a
Paul Phan <phanminh65@gmail.com>
2018-07-24 09:58:55 +0700

    Update dependencies

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,11 +1,9 @@
 import winston from "winston";
-import { Logger } from "winston";
-import { ENVIRONMENT } from "./secrets";
 
-const logger = new (Logger)({
+const logger = winston.createLogger({
     transports: [
-        new (winston.transports.Console)({ level: process.env.NODE_ENV === "production" ? "error" : "debug" }),
-        new (winston.transports.File)({ filename: "debug.log", level: "debug"})
+        new (winston.transports.Console)({level: process.env.NODE_ENV === "production" ? "error" : "debug"}),
+        new (winston.transports.File)({filename: "debug.log", level: "debug"})
     ]
 });
 
