89f3e048680574c7aeb0e8fcfab8cc0bc55a2e5f
Bowden Kelly <wilkelly@microsoft.com>
2019-03-25 14:56:30 -0700

    update mongoose to v5

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,7 @@ const app = express();
 // Connect to MongoDB
 const mongoUrl = MONGODB_URI;
 (<any>mongoose).Promise = bluebird;
-mongoose.connect(mongoUrl, {useMongoClient: true}).then(
+mongoose.connect(mongoUrl).then(
   () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
 ).catch(err => {
   console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
