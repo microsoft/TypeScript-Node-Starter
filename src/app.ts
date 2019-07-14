3414ceca45dd9d782d10ec72461635d0540433d8
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-07-14 22:45:19 +0200

    Migrate TSLint to ESLint. Closes #209
    This commit rewrites linting support in the project to be based solely
    on the ESLint as advised by the TSLint tool authors:
    https://medium.com/palantir/tslint-in-2019-1a144c2317a9
    
    The migration is based on default, recommended settings for TypeScript
    in ESLint and is expected to be updated in future to better fit project
    goals.
    
    All references has been updated and replaced with relevant ESLint
    context:
    
    - dependencies migration from TSLint to ESLint
    - VSCode configuration changes to support ESLint exension
    - VSCode extensions recommendation changes
    - `.eslintrc` and `.eslintignore` configuration files added
    - all error level problems in the source files are covered by this
      migration
    
    Thanks!

--- src/app.ts
+++ src/app.ts
@@ -29,13 +29,13 @@ const app = express();
 
 // Connect to MongoDB
 const mongoUrl = MONGODB_URI;
-(<any>mongoose).Promise = bluebird;
+mongoose.Promise = bluebird;
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true} ).then(
-  () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
 ).catch(err => {
-  console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-  // process.exit();
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    // process.exit();
 });
 
 // Express configuration
@@ -46,13 +46,13 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: SESSION_SECRET,
-  store: new MongoStore({
-    url: mongoUrl,
-    autoReconnect: true
-  })
+    resave: true,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    store: new MongoStore({
+        url: mongoUrl,
+        autoReconnect: true
+    })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -60,26 +60,26 @@ app.use(flash());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
 app.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
+    res.locals.user = req.user;
+    next();
 });
 app.use((req, res, next) => {
-  // After successful login, redirect back to the intended page
-  if (!req.user &&
+    // After successful login, redirect back to the intended page
+    if (!req.user &&
     req.path !== "/login" &&
     req.path !== "/signup" &&
     !req.path.match(/^\/auth/) &&
     !req.path.match(/\./)) {
-    req.session.returnTo = req.path;
-  } else if (req.user &&
+        req.session.returnTo = req.path;
+    } else if (req.user &&
     req.path == "/account") {
-    req.session.returnTo = req.path;
-  }
-  next();
+        req.session.returnTo = req.path;
+    }
+    next();
 });
 
 app.use(
-  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
 
 /**
@@ -114,7 +114,7 @@ app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthor
  */
 app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "public_profile"] }));
 app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
-  res.redirect(req.session.returnTo || "/");
+    res.redirect(req.session.returnTo || "/");
 });
 
 export default app;
