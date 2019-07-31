3e2f6c0c517454a125888de6588c579f7ec0ae26
Mathias Rasmussen <mathiasvr@gmail.com>
2019-07-31 12:34:57 +0200

    Fix lint warnings

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-	globals: {
-		'ts-jest': {
-			tsConfig: 'tsconfig.json'
-		}
-	},
-	moduleFileExtensions: [
-		'ts',
-		'js'
-	],
-	transform: {
-		'^.+\\.(ts|tsx)$': 'ts-jest'
-	},
-	testMatch: [
-		'**/test/**/*.test.(ts|js)'
-	],
-	testEnvironment: 'node'
+    globals: {
+        "ts-jest": {
+            tsConfig: "tsconfig.json"
+        }
+    },
+    moduleFileExtensions: [
+        "ts",
+        "js"
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    testMatch: [
+        "**/test/**/*.test.(ts|js)"
+    ],
+    testEnvironment: "node"
 };
