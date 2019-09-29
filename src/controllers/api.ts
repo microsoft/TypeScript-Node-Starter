1409fd7164e4320d809094ae7d338680e14fc1cd
CarlosK <CarlosSolrac@users.noreply.github.com>
2019-09-29 05:31:07 -0500

    Update packages, add tests, add type casts (#229)
    - upgrade to latest packages
    - cast `req.user` as `UserDocument`
    - update logger to use latest Winston syntax
    - disable `no-inferrable-types`, Mongoose use unified topology
    
    Thx to @CarlosSolrac

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,6 +2,7 @@
 
 import graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
+import { UserDocument } from "../models/User";
 
 
 /**
@@ -19,9 +20,10 @@ export const getApi = (req: Request, res: Response) => {
  * Facebook API example.
  */
 export const getFacebook = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.user.tokens.find((token: any) => token.kind === "facebook");
+    const user = req.user as UserDocument;
+    const token = user.tokens.find((token: any) => token.kind === "facebook");
     graph.setAccessToken(token.accessToken);
-    graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
+    graph.get(`${user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
         if (err) { return next(err); }
         res.render("api/facebook", {
             title: "Facebook API",
