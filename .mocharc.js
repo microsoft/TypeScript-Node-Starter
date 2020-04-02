cbfe4eaacddac808293add78b538ab0ab53e2dbb
Matt Tyler <matt.tyler@mechanicalrock.io>
2020-04-02 13:47:39 +0800

    Add linting

--- .mocharc.js
+++ .mocharc.js
@@ -2,6 +2,6 @@ module.exports = {
     extension: [ "ts" ],
     spec: "test/**/*.spec.ts",
     require: "test/babel-register.js",
-    // reporter: "mochawesome",
-    // "reporter-options": "reportDir=.test"
+    reporter: "mochawesome",
+    "reporter-options": "reportDir=.test"
 }
