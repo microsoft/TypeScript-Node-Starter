733ec33e68093650b1fa5784734227fa5d682ee6
Jatupon Sukkasem <jasukkas.th@gmail.com>
2020-06-24 12:42:18 +0700

    Added data inserting example.

--- src/controllers/Home.ts
+++ src/controllers/Home.ts
@@ -3,9 +3,13 @@
 
 import {Request, Response} from "express";
 import Componentindex from "./components/index.js";
+import Component829ab3c5 from "./components/_829ab3c5.js";
 
 export const index = (req: Request, res: Response) => {
 	new Componentindex(req, res, "home/index");
+}
+export const _829ab3c5 = (req: Request, res: Response) => {
+	new Component829ab3c5(req, res, "home/_829ab3c5");
 }
 
 // <--- Auto[Generating:V1]
