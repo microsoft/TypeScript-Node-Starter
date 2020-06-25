4c07955b93564844a88d7a8cae21468c14ca3aa7
Jatupon Sukkasem <jasukkas.th@gmail.com>
2020-06-25 11:35:02 +0700

    Added a component demo.

--- src/controllers/Home.ts
+++ src/controllers/Home.ts
@@ -5,6 +5,7 @@ import {Request, Response} from "express";
 import Componentindex from "./components/index.js";
 import Component829ab3c5 from "./components/_829ab3c5.js";
 import Componentb0bd3a3e from "./components/_b0bd3a3e.js";
+import Component39ee7bcc from "./components/_39ee7bcc.js";
 
 export const index = (req: Request, res: Response) => {
 	new Componentindex(req, res, "home/index");
@@ -14,6 +15,9 @@ export const _829ab3c5 = (req: Request, res: Response) => {
 }
 export const _b0bd3a3e = (req: Request, res: Response) => {
 	new Componentb0bd3a3e(req, res, "home/_b0bd3a3e");
+}
+export const _39ee7bcc = (req: Request, res: Response) => {
+	new Component39ee7bcc(req, res, "home/_39ee7bcc");
 }
 
 // <--- Auto[Generating:V1]
---
+++ src/public/js/components/FlowLayout_5382c791.tsx
@@ -0,0 +1,107 @@
+// Auto[Generating:V1]--->
+    // PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
+    
+// Auto[Import]--->
+import {Project, DeclarationHelper} from '../helpers/DeclarationHelper.js';
+import {CodeHelper} from '../helpers/CodeHelper.js';
+import {EventHelper} from '../helpers/EventHelper.js';
+import {IBaseProps, IBaseState, DefaultBaseProps, DefaultBaseState, Base} from './Base.js';
+// <---Auto[Import]
+
+// Import additional modules here:
+//
+
+// Auto[Declare]--->
+
+declare let React: any;
+declare let ReactDOM: any;
+declare let window: any;
+
+// <---Auto[Declare]
+
+// Declare private static variables here:
+//
+
+// Auto[Interface]--->
+interface IAutoBaseProps extends IBaseProps {
+  forward: {classes: String, styles: any};
+}
+interface IAutoBaseState extends IBaseState {
+}
+// <---Auto[Interface]
+
+// Declare or extend interfaces here:
+//
+interface IProps extends IAutoBaseProps {
+  
+}
+interface IState extends IAutoBaseState { 
+}
+
+let DefaultProps = Object.assign({}, DefaultBaseProps, {
+  
+});
+let DefaultState = Object.assign({}, DefaultBaseState, {
+  
+});
+
+// Auto[ClassBegin]--->
+class FlowLayout_5382c791 extends Base {
+  state: IState = null;
+  protected static defaultProps: IProps = DefaultProps;
+  
+  constructor(props) {
+    super(props);
+    this.state = CodeHelper.clone(DefaultState);
+    
+    this.initialize();
+  }
+  // <---Auto[ClassBegin]
+  
+  // Declare class variables and functions here:
+  //
+  protected initialize(): void {
+  }
+  
+  // Providing data array base on dot notation:
+  // 
+  protected getDataFromNotation(notation: string): any {
+    return super.getDataFromNotation(notation);
+  }
+  
+  // Auto[Merging]--->
+  // <---Auto[Merging]
+  
+  // Auto[ClassEnd]--->
+  protected render(): any {
+    return (
+      <div className={"internal-fsb-element " + (this.props.forward && this.props.forward.classes || '')} internal-fsb-guid="5382c791" style={Object.assign({'minHeight': '200px', 'FsbBackgroundType': 'linear', 'background': 'linear-gradient(90deg, rgba(3, 115, 252, 1) 0%, rgba(255, 0, 85, 1) 100%)'}, this.props.forward && this.props.forward.styles || {})}>
+        <div className="container-fluid">
+          <div className="row internal-fsb-strict-layout internal-fsb-allow-cursor">
+            <div className="internal-fsb-element col-12 offset-0" internal-fsb-guid="7e72dc95" style={{'color': 'rgba(255, 255, 255, 0.83)', 'textAlign': 'center'}}>
+              <div>
+                <br />
+              </div>
+              <div>
+                <br />
+              </div>
+              <div>
+                <br />
+              </div>
+              This is a component which can be duplicated.
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+DeclarationHelper.declare('Site', 'Controls.FlowLayout_5382c791', FlowLayout_5382c791);
+// <---Auto[ClassEnd]
+
+// Export variables here:
+//
+export {IProps, IState, DefaultProps, DefaultState};
+
+    // <--- Auto[Generating:V1]
+    // PLEASE DO NOT MODIFY BECAUSE YOUR CHANGES MAY BE LOST.
