fd4c70810c8cf6991f3a8a5ad144222fb76baa81
Jatupon Sukkasem <jatupon@softenstorm.com>
2020-07-04 21:54:23 +0700

    Increased upload size to 50mb.

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,8 @@ app.set("port", process.env.PORT || 8000);
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
 app.use((req, res, next) => {
