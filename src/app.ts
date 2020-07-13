954a8cbd000b1ef72db3b47ba25fb2ced44f3657
Nyasha (Nash) Nziramasanga <47994666+NyashaNziramasanga@users.noreply.github.com>
2020-07-14 06:01:14 +0930

    Using template literals for mongodb error message (#298)
    thx to @NyashaNziramasanga

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ mongoose.Promise = bluebird;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
     () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
 ).catch(err => {
-    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
     // process.exit();
 });
 
